f4cd80a52a4dec0f5cd3fa82b041a5c75f48d1df
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-06 20:58:23 -0700

    add new app.js file

---
+++ app.js
@@ -0,0 +1,21 @@
+const input = document.getElementById('user-input');
+
+
+// input.addEventListener('click', () => {
+
+//     // post-click logic goes here!
+//     inputElement.textContent = 'userInput'
+
+//     })
+
+
+    const myButton = document.getElementById('buttonOne');
+    console.log(myButton)
+
+    myButton.addEventListener('click', () => {
+        console.log('Im in here')
+
+        // post-click logic goes here!
+    //    myButton.textContent = 'Welcome Home!'
+    //     myButton.inputElement = 'userInput'
+    })
