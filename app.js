e2a6d0412fa7f04dc2e12a37a6111d789f6d343e
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-07 00:02:56 -0700

    new color changes and buttons added

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const input = document.getElementById('user-input');
+const middleInput = document.getElementById('middle_div');
+const myButton = document.getElementById('buttonOne');
+const buttonM = document.getElementById('buttonM');
+const buttonC = document.getElementById('buttonC');
+const buttonY = document.getElementById('buttonY');
+const toppy = document.getElementsByClassName('top')
+const bottomy = document.getElementsByClassName('bottom')
+const pig = document.getElementById('L1')
 
+myButton.addEventListener('click', () => {
+    middleInput.innerHTML = input.value
+})
 
-// input.addEventListener('click', () => {
+buttonM.addEventListener('click', () => {
+    toppy[0].style.backgroundColor = "magenta";
+    bottomy[0].style.backgroundColor ="magenta";
+    pig.style.color = "white"
 
-//     // post-click logic goes here!
-//     inputElement.textContent = 'userInput'
+})
 
-//     })
+buttonC.addEventListener('click', () => {
+    toppy[0].style.backgroundColor = "rgb(0,310,330)"
+    bottomy[0].style.backgroundColor = "rgb(0,310,330)"
+    pig.style.color = "black"
+})
 
-
-    const myButton = document.getElementById('buttonOne');
-    console.log(myButton)
-
-    myButton.addEventListener('click', () => {
-        console.log('Im in here')
-
-        // post-click logic goes here!
-    //    myButton.textContent = 'Welcome Home!'
-    //     myButton.inputElement = 'userInput'
-    })
+buttonY.addEventListener('click', () => {
+    toppy[0].style.backgroundColor ="#FFFF00"
+    bottomy[0].style.backgroundColor = "#FFFF00"
+    pig.style.color = "black"
+ 
+})
